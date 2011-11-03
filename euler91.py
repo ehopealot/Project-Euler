85ee581dd65f70d49fe018ca8461c02f8a369849
Erik Hope <erikjhope@gmail.com>
2011-11-02 20:53:39 -0500

    Changes to 91 and 94

--- euler91.py
+++ euler91.py
@@ -4,32 +4,32 @@ from fractions import Fraction
 
 m = 50
 
-
+count = 0
 points = Set()
 for y in range(m, 0, -1):
     for x in range(1, m+1):
         points.add((0,y,x,0))
+        count += 1
 
 for y in range(m, 0, -1):
     for x in range(m, 0, -1):
         points.add((x,y,x,0))
-
+        count += 1
 for y in range(m, 0, -1):
     for x in range(1, m+1):
         points.add((0,y,x,y))
-
+        count += 1
 
 for y in range(m, 0, -1):
     for x in range(0, m+1):
-        print x,y
         slope = Fraction(-x,y)
-        print x,y,slope
         other_y = y
         for other_x in range(x+1, m+1):
             other_y += slope
             if other_y < 0: break
             if other_y % 1 == 0:
                 points.add((x,y,other_x,other_y))
+                count += 1
         other_y = y
 
         for other_x in range(x-1, -1, -1):
@@ -37,7 +37,8 @@ for y in range(m, 0, -1):
             if other_y > m: break
             if other_y %1 == 0:
                 points.add((x,y,other_x,other_y))
+                count += 1
 
 
 
-print  len(points)
+print  len(points), count
--- euler94.py
+++ euler94.py
@@ -1,27 +1,57 @@
-# Pythagorean triples: m^2 - n^2 : 2*m*n : m^2 + n^2
+# b = a+-1
+# c = a
+# A = (1/2)*sqrt(a^4-()(2a^2 - (a-1)^2)/2)
+#
+#
+
 import math
 
 def do_94():
 
-    top = 167000000
+#    top = 167000000
+    top = 10000
     result = 0
     counter = 0
     smaller = True
-    for a in xrange(3, top):
-        if a% 10000000 == 0: print a
-        c = a*2-1
-        b_squared = c**2 - a**2
-        b = math.sqrt(b_squared)
-        if b%1 == 0:
-            print a*2, c, c
-            result += a*2 + c*2
-        else:
-            c = a*2+1
-            b_squared = c**2 - a**2
-            b = math.sqrt(b_squared)
-            if b%1 ==0:
-                print a*2, c, c
-                result += a*2 + c*2
+
+    for c in xrange(5,top):
+        if c%10000000 == 0 : print c
+        if c%2 == 0: continue
+
+        a = .5
+        if (c-1)%4 == 0:
+            a = ((c-1)/4)*math.sqrt((3*c-1)*(c+1))
+
+        if a%1 == 0:
+ 
+            print c, c, c-1
+            result += c-1+c+c
+
+#           print b, c, c
+        a = .5
+        if (c+1)%4 == 0:
+            a = ((c+1)/4)*math.sqrt((3*c+1)*(c-1))
+        if a%1 == 0:
+ 
+            print c, c, c+1
+            result += c+c+c+1
+    return result
+
+#     for a in xrange(3, top):
+#         if a% 10000000 == 0: print a
+#         c = a*2-1
+#         b_squared = c**2 - a**2
+#         b = math.sqrt(b_squared)
+#         if b % 1 == 0:
+#             print a*2, c, c, a*2+c*2
+#             result += a*2 + c*2
+#         else:
+#             c = a*2+1
+#             b_squared = c**2 - a**2
+#             b = math.sqrt(b_squared)
+#             if b % 1 == 0:
+#                 print a*2, c, c, a*2 + c*2
+#                 result += a*2 + c*2
                           
         
     return result
